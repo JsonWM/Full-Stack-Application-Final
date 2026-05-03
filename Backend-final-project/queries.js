45d079cdc93aec1bf00e98e94f2a85e28ee516d2
JsonWM <usermanage.py@gmail.com>
2026-05-02 22:46:25 -0400

    Setting for deployment:Backend in the cloud

--- Backend-final-project/queries.js
+++ Backend-final-project/queries.js
@@ -2,9 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  //For deployment
-  // ssl: { rejectUnauthorized: false } 
+  connectionString: process.env.DATABASE_URL,  
+  ssl: { rejectUnauthorized: false } 
 });
 
 pool.on('connect', () => {
